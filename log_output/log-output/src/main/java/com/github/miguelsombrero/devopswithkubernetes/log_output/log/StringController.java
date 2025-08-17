f4549cf184d552951cedf8e9d2de5b6ae5e6e878
Miika Somero <miika.somero@gmail.com>
2025-08-17 07:29:57 +0300

    exercise 1.7 added service and ingress for log-output-app and added endpoint for requesting random string

---
+++ log_output/log-output/src/main/java/com/github/miguelsombrero/devopswithkubernetes/log_output/log/StringController.java
@@ -0,0 +1,22 @@
+package com.github.miguelsombrero.devopswithkubernetes.log_output.log;
+
+import com.github.miguelsombrero.devopswithkubernetes.log_output.util.RandomStringLogger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/string")
+public class StringController {
+
+    private final RandomStringLogger stringLogger;
+
+    public StringController(RandomStringLogger stringLogger) {
+        this.stringLogger = stringLogger;
+    }
+
+    @GetMapping
+    public String getRandomString() {
+        return stringLogger.getRandomString();
+    }
+}
--- log_output/log-output/src/main/java/com/github/miguelsombrero/devopswithkubernetes/log_output/util/RandomStringLogger.java
+++ log_output/log-output/src/main/java/com/github/miguelsombrero/devopswithkubernetes/log_output/util/RandomStringLogger.java
@@ -1,24 +1,22 @@
 package com.github.miguelsombrero.devopswithkubernetes.log_output.util;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
 import java.util.UUID;
 
 @Slf4j
+@Getter
 @Component
 public class RandomStringLogger {
 
-    private final String randomString;
+    private String randomString;
 
     public RandomStringLogger() {
-        randomString = UUID.randomUUID().toString().substring(0, 20);
+        randomString = Instant.now().toString() + ": " + UUID.randomUUID();
         log.info("Generated random string: {}", randomString);
     }
 
-    @Scheduled(fixedRate = 5000)
-    public void print() {
-        log.info(randomString);
-    }
 }
